89767950393e26595216c6ec3b5a8a89591e3ade
Kathryn <dorge.k46@gmail.com>
2024-12-02 20:47:53 +1000

    day2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "./aoc/day01.h"
+#include "./aoc/day02.h"
 #include "./util/util.h"
 
 // #include <string>
 
 int main(int arc, char** argv)
 {
-    day01::partOne("puzzleInput/day1.txt");
-    day01::partTwo("puzzleInput/day1.txt");
+    day02::partOne("puzzleInput/day2.txt");
+    day02::partTwo("puzzleInput/day2.txt");
 }
